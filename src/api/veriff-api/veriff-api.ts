9cdfa8404f6799564534808f8430c6c8b7cab5ad
alejandro.tidele <alejandro.tidele@glovoapp.com>
2024-02-16 22:25:39 +0100

    Add all Veriff internal endpoints

--- src/api/veriff-api/veriff-api.ts
+++ src/api/veriff-api/veriff-api.ts
@@ -1,10 +1,41 @@
 import client from './client';
+import type { MediaContext, Session, SessionMedia } from './types';
 
 export const getSessionBySessionId = async (
   sessionId: string,
-): Promise<unknown> => {
+): Promise<Session> => {
   try {
-    const { data } = await client.get(`/sessions/${sessionId}`);
+    const { data } = await client.get<Session>(`/sessions/${sessionId}`);
+    return data;
+  } catch (error) {
+    // Monitor this log in Datadog
+    console.error(error);
+    throw error;
+  }
+};
+
+export const getSessionMediaBySessionId = async (
+  sessionId: string,
+): Promise<SessionMedia> => {
+  try {
+    const { data } = await client.get<SessionMedia>(
+      `/sessions/${sessionId}/media`,
+    );
+    return data;
+  } catch (error) {
+    // Monitor this log in Datadog
+    console.error(error);
+    throw error;
+  }
+};
+
+export const getMediaContextBySessionId = async (
+  sessionId: string,
+): Promise<MediaContext> => {
+  try {
+    const { data } = await client.get<MediaContext>(
+      `/media-context/${sessionId}`,
+    );
     return data;
   } catch (error) {
     // Monitor this log in Datadog
